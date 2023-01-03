2737c10220f0a4ce56318b78d893397812b0f6da
Akkiwadhwa <114605480+Akkiwadhwa@users.noreply.github.com>
2023-01-03 19:04:54 +0530

    2

--- main.py
+++ main.py
@@ -20,10 +20,10 @@ headers = {
 cookies = {
     '_session_id': 'VkpzZjhpZk95QVFhbk5WMFZ3bXhpai9vNjI4TE1vUC9IYW1PZFZzTlA0UG5GRWJtOVJ4M2lmLzlDUUxITTV2VVdhKzVGMmdGaHdUclo4ejExcVA3bEZtZTNvZGgzVW5TcFoyN21TY3U4L3B4RENUUUdMaDIyMFZSYTVmSXpOenBnZWRkZlNzTU80ZkJUUVp4NTlFY0x4LzZkMEZtR0ltaHhIeWZxcm9ocWdxR1h0U2lCU2Zmb2Y4T05oRlBrSlluLS0vc1ppUjgwam1jOXdsejRhOGdRSXZRPT0%3D--2547c4e8925e162a172ef9f417de2d6d22ad43f4',
 }
-l = []
 
 
 def scrap(x):
+    global dict1
     r = requests.get(f"https://api.brownbook.net/app/api/v1/businesses/resource/{x}/revision", headers=headers,
                      cookies=cookies)
     data = r.json()
@@ -72,11 +72,20 @@ def scrap(x):
             "Website": website,
             "Page url": url
         }
-        l.append(dict1)
+    return dict1
 
 
-my_iter = range(1, 1000)
-with concurrent.futures.ThreadPoolExecutor(32) as executor:
-    results = list(tqdm(executor.map(scrap, my_iter), total=len(my_iter)))
-df = pd.DataFrame(l)
+def run(f, my_iter):
+    with concurrent.futures.ThreadPoolExecutor(32) as executor:
+        results = list(tqdm(executor.map(f, my_iter), total=len(my_iter)))
+    return results
+
+
+final = []
+for i in range((2000 // 1000)):
+    print(i)
+    my_iter = range(1000 * i, 1000 * (i + 1))
+    final += run(scrap, my_iter)
+
+df = pd.DataFrame(final)
 df.to_csv("data1.csv")
