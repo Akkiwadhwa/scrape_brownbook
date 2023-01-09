e23c8fdeb840c6d24fd16735e8ba0d6192cb87c6
Akkiwadhwa <114605480+Akkiwadhwa@users.noreply.github.com>
2023-01-09 12:00:16 +0530

    4

--- main.py
+++ main.py
@@ -1,37 +1,35 @@
 import concurrent.futures
-
+import json
 import pandas as pd
 import requests
+import urllib3
 from tqdm import tqdm
 
 requests.urllib3.disable_warnings()
 
 headers = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'Accept-Language': 'en-US,en;q=0.9',
-    'Connection': 'keep-alive',
     'Referer': 'https://brownbook.net',
     'Sec-Fetch-Dest': 'document',
     'Sec-Fetch-Mode': 'navigate',
-    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-Site': 'none',
     'Sec-Fetch-User': '?1',
-    'Sec-GPC': '1',
     'Upgrade-Insecure-Requests': '1',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
-}
-
-cookies = {
-    '_session_id': 'VkpzZjhpZk95QVFhbk5WMFZ3bXhpai9vNjI4TE1vUC9IYW1PZFZzTlA0UG5GRWJtOVJ4M2lmLzlDUUxITTV2VVdhKzVGMmdGaHdUclo4ejExcVA3bEZtZTNvZGgzVW5TcFoyN21TY3U4L3B4RENUUUdMaDIyMFZSYTVmSXpOenBnZWRkZlNzTU80ZkJUUVp4NTlFY0x4LzZkMEZtR0ltaHhIeWZxcm9ocWdxR1h0U2lCU2Zmb2Y4T05oRlBrSlluLS0vc1ppUjgwam1jOXdsejRhOGdRSXZRPT0%3D--2547c4e8925e162a172ef9f417de2d6d22ad43f4',
 }
 
 
 def scrap(x):
     global dict1
-    r = requests.get(f"https://api.brownbook.net/app/api/v1/businesses/resource/{x}/revision", headers=headers,
-                     cookies=cookies, verify=False)
-    data = r.json()
+    dict1 = {}
     tag = ""
     try:
+        http = urllib3.PoolManager(cert_reqs='CERT_NONE')
+        r = http.request("GET", url=f"https://api.brownbook.net/app/api/v1/businesses/resource/{x}/revision",
+                         headers=headers)
+        data = json.loads(r.data.decode('utf-8'))
+
         bussiness_name = data["data"]["metadata"]["name"]
         c_name = data["data"]["metadata"]["claimed"]
         if c_name == True:
@@ -53,11 +51,6 @@ def scrap(x):
         email = data["data"]["metadata"]["email"]
         website = data["data"]["metadata"]["website"]
         url = f"https://www.brownbook.net/business/{data['data']['id']}/{data['data']['metadata']['link']}"
-
-
-    except:
-        pass
-    else:
         dict1 = {
             "Bussiness Name": bussiness_name,
             "Contact Name": contact_name,
@@ -75,20 +68,36 @@ def scrap(x):
             "Website": website,
             "Page url": url
         }
+    except:
+        dict1 = {}
+
     return dict1
 
 
-def run(f, my_iter):
-    with concurrent.futures.ThreadPoolExecutor(32) as executor:
-        results = list(tqdm(executor.map(f, my_iter), total=len(my_iter)))
-    return results
-
-
-final = []
-for i in range((2000 // 1000)):
-    print(i)
-    my_iter = range(1000 * i, 1000 * (i + 1))
-    final += run(scrap, my_iter)
-
-df = pd.DataFrame(final)
-df.to_csv("data1.csv")
+print("Welcome to the Scrapper")
+from_ = int(input("From: "))
+to_ = int(input("To: "))
+if isinstance(from_, int) and isinstance(to_, int):
+    for i in range(from_, to_):
+        print(i)
+        my_iter = range(10000 * i, 10000 * (i + 1))
+        with concurrent.futures.ThreadPoolExecutor(16) as executor:
+            results = list(tqdm(executor.map(scrap, my_iter), total=len(my_iter)))
+        df = pd.DataFrame(results)
+        try:
+            a = df.sort_values('Country Code')
+            a.to_csv(f"{10000 * i}-{10000 * (i + 1)}.csv")
+        except Exception as e:
+            print(f"CSV is blank {e}")
+else:
+    print("Program Exit.")
+# import time
+#
+# import pyautogui
+#
+# while True:
+#     time.sleep(3)
+#     pyautogui.moveRel(0, 50, duration = 1)
+#     pyautogui.moveTo(1000, 800, duration=1)
+#     pyautogui.click()
+#     pyautogui.moveRel(50, 0, duration=1)
